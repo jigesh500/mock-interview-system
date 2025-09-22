2e898658d3ceeeea90739b19414f8832ba4f0e24
Dimpal <Dimpal.Rathod@msbcgroup.com>
2025-09-22 12:48:19 +0530

    setup frontend with Vite - React - TS - MUI - Tailwind css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-500 min-h-screen flex items-center justify-center'>
+      <Typography variant="h1">Hiring!</Typography>
+    </div>
+  )
+}
+
+export default App
